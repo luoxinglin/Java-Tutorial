09644a7245b5a1c7f37ded175f3b92abc59fa348
luoxinglin <1978771249@qq.com>
2019-06-03 15:53:38 +0800

    java Number和Math类

---
+++ src/com/java/number/NumberTest.java
@@ -0,0 +1,12 @@
+package com.java.number;
+
+/**
+ * @author luo xinglin
+ * @date 2019/6/3 - 15:49
+ */
+public class NumberTest {
+    Integer
+    //所有的包装类（Integer、Long、Byte、Double、Float、Short）都是抽象类Number的子类。
+    //Java 的 Math 包含了用于执行基本数学运算的属性和方法，如初等指数、对数、平方根和三角函数。
+    //Math 的方法都被定义为 static 形式，通过 Math 类可以在主函数中直接调用。
+}
